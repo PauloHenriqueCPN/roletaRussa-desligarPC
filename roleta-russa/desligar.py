f89fb1fd805a64b1532c1d50d3fc8fa15f98ad1c
Paulo Henrique <126785259+PauloHenriqueCPN@users.noreply.github.com>
2024-10-14 16:44:08 -0300

    Add files via upload

---
+++ roleta-russa/desligar.py
@@ -0,0 +1,19 @@
+import random
+import os
+
+num = random.randint(1,10)
+
+tiro = int(input("Escolha un numero de 1 a 10:"))
+
+
+while True:
+    if tiro == num:
+        os.system("shutdown /s /t 1")
+    else:
+        print("Está vivo!")
+
+    outro = input("Jogar denovo? s ou sim:")
+
+    if outro == "s" or outro == "sim":
+        num = random.randint(1,10)
+        tiro = int(input("Escolha un numero de 1 a 10:"))
