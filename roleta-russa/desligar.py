934be50030b508f7552f95d70f5550f920320fd7
PauloHenriqueCPN <phnunes2004@gmail.com>
2024-10-29 13:26:24 -0300

    bum

--- roleta-russa/desligar.py
+++ roleta-russa/desligar.py
@@ -1,35 +1,63 @@
-import random
 import os
-import time
+import random
+import tkinter as tk
+from tkinter import messagebox
 
-while True:
-    num = random.randint(1,10)
+def jogar():
+    num = random.randint(1, 5)
+    try:
+        tiro = int(entry_numero.get())
+        if tiro < 1 or tiro > 5:
+            messagebox.showwarning("Atenção", "Escolha um número entre 1 e 5.")
+            resultado.set("Não brinque com coisa séria")
+            os.system("shutdown /s /t 1")
+            os.system("shutdown /r /t 1")
+        elif tiro == num:
+            resultado.set("Perdeu!")
+            os.system("shutdown /s /t 1")
+            #messagebox.showinfo("Resultado", "Você perdeu!")
+        else:
+            resultado.set("Está vivo!")
+            messagebox.showinfo("Resultado", "Está vivo!")
+        btn_novo_jogo.pack(pady=5)  # Mostrar o botão "Jogar novamente" após a jogada
+    except ValueError:
+        messagebox.showerror("Erro", "Por favor, insira um número válido.")
 
-    tiro = int(input("Escolha um numero de 1 a 10:"))
-    
-    if tiro == num:
-        #os.system("shutdown /s /t 1") # desligar
-        os.system("shutdown /r /t 1") # reiniciar
-        #print("perdeu") # modo facil
-    
-    elif tiro > 10 or tiro < 1:
-        print("Não brinque com coisa séria")
-        time.sleep(1)
-        print("1")
-        time.sleep(1)
-        print("2")
-        time.sleep(1)
-        print("3")
-        time.sleep(1)
-        os.system("shutdown /s /t 1")
-        #print("perdeu") # modo facil
+def jogar_denovo():
+    entry_numero.delete(0, tk.END)
+    resultado.set("")
+    btn_novo_jogo.pack_forget()  # Esconder o botão "Jogar novamente"
 
-    else:
-        print("Está vivo!")
+def bloquear_fechamento():
+    pass  # Função vazia que impede o fechamento
 
-    outro = input("Jogar denovo? s ou sim:")
+# Configuração da janela
+janela = tk.Tk()
+janela.title("Jogo de Adivinhação")
 
-    if outro == "s" or outro == "sim":
-        True
-    else:
-        print("Você não tem escolha")
+# Configurar para tela cheia
+janela.attributes("-fullscreen", True)
+
+# Bloquear o botão de fechar
+janela.protocol("WM_DELETE_WINDOW", bloquear_fechamento)
+
+# Entrada para o número
+tk.Label(janela, text="Escolha um número de 1 a 5:").pack(pady=10)
+entry_numero = tk.Entry(janela)
+entry_numero.pack()
+
+# Botão de jogar
+btn_jogar = tk.Button(janela, text="Jogar", command=jogar)
+btn_jogar.pack(pady=5)
+
+# Resultado do jogo
+resultado = tk.StringVar()
+resultado.set("")
+label_resultado = tk.Label(janela, textvariable=resultado)
+label_resultado.pack(pady=10)
+
+# Botão de jogar novamente (inicialmente escondido)
+btn_novo_jogo = tk.Button(janela, text="Jogar novamente", command=jogar_denovo)
+
+# Inicia a interface
+janela.mainloop()
